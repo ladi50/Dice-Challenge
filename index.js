474760ce2ac2c8017f80dbf3fed6aef08ac01ed4
Your Name <ladi50@walla.com>
2020-07-02 18:45:35 +0300

    Add app files

---
+++ index.js
@@ -0,0 +1,19 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+function randomDice () {
+	document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+	document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+}
+
+randomDice();
+
+if (randomNumber1 > randomNumber2) {
+	document.querySelector("h1").innerHTML = "🎉 Player 1 Wins!";
+
+}	else if (randomNumber1 < randomNumber2) {
+	document.querySelector("h1").innerHTML = "Player 2 Wins! 🎉";
+
+} else {
+	document.querySelector("h1").innerHTML = "✨ It's a Draw! ✨";
+}
